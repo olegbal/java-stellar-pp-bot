918070d4bf719c41ce25a5ed5ec8e3fff88981c7
olegbal <olegbalah@gmail.com>
2023-10-03 20:07:05 +0400

    Initial commit

---
+++ src/main/java/com/github/olegbal/javastellarppbot/javastellarppbot/JavaStellarPpBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.olegbal.javastellarppbot.javastellarppbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStellarPpBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStellarPpBotApplication.class, args);
+    }
+
+}
