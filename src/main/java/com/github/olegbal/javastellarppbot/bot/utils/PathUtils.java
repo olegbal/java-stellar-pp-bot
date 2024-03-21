c593b73a1b9f556d6e5fa0b9393305ee394c2e50
olegbal <olegbalah@gmail.com>
2024-03-21 22:40:25 +0400

    Fix NPE

--- src/main/java/com/github/olegbal/javastellarppbot/bot/utils/PathUtils.java
+++ src/main/java/com/github/olegbal/javastellarppbot/bot/utils/PathUtils.java
@@ -6,11 +6,13 @@ import org.stellar.sdk.AssetTypeCreditAlphaNum;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.github.olegbal.javastellarppbot.bot.utils.AssetUtils.getCode;
+
 public class PathUtils {
 
     public static String buildStringPath(List<Asset> paths, String delimeter) {
         return paths.stream()
-                .map(asset -> ((AssetTypeCreditAlphaNum) asset).getCode())
+                .map(AssetUtils::getCode)
                 .collect(Collectors.joining(delimeter));
     }
 }
